39861949e7864348edb242c30b7ccf117537fad3
ZanhNe <baodoanh1102004@gmail.com>
2026-07-23 07:15:29 +0700

    first commit

---
+++ backend/route-sharing/src/main/java/com/zanh/route_sharing/RouteSharingApplication.java
@@ -0,0 +1,13 @@
+package com.zanh.route_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteSharingApplication.class, args);
+	}
+
+}
---
+++ backend/route-sharing/src/test/java/com/zanh/route_sharing/RouteSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanh.route_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
